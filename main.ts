d37072cdc23466099647b0a508861f86132b7273
Muhammad Saqib <Mughalsaqib957@gmail.com>
2024-07-25 16:12:51 +0500

    pushed the code

---
+++ main.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "UserguessedNumber",
+        type : "number",
+        message : "please select number b/w 1 - 10"
+    }
+])
+if (answer.UserguessedNumber === randomNumber) {
+    console.log("you guessed it right")
+}
+else{console.log("you guessed wrong")
+}
